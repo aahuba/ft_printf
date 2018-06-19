a21a5017bd49a0bb15d164779c19aab46c48cd69
aahuba <huba.zp@gmail.com>
2018-06-19 13:46:04 +0300

    Update ft_format_o.c

--- ftprintf/ft_format_o.c
+++ ftprintf/ft_format_o.c
@@ -11,7 +11,7 @@ void	ft_format_o(va_list ap, t_printf *all, int *pd, char c)
 		a = va_arg(ap, unsigned int) & 0xFFFF;
 	else if (all->l_m == 2)
 		// a = va_arg(ap, unsigned short int) & 0xFF;
-		a = (unsigned char)(a);
+		a = (unsigned char)(a) & 0xFF;
 	else if (all->l_m == 3)
 		a = va_arg(ap, unsigned long);
 	else if (all->l_m == 4)
