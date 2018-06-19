460c09e9cf8dae06b1a61690bdbc889e2ca0a6a1
aahuba <huba.zp@gmail.com>
2018-06-19 13:54:22 +0300

    Add files via upload

--- ftprintf/ft_format_u.c
+++ ftprintf/ft_format_u.c
@@ -2,31 +2,31 @@
 
 void	ft_format_u(va_list ap, t_printf *all, int *pd)
 {
-	unsigned long long int *a;
+	unsigned long long int a;
 
-	a = va_arg(ap, unsigned long long int*);
+	a = va_arg(ap, unsigned long long int);
 	if (all->l_m == 1)
 		// a = va_arg(ap, unsigned int) & 0xFFFF;
-		*a = (unsigned int)(*a);
+		a = (unsigned int)(a) & 0xFFFF;
 	else if (all->l_m == 2)
 		// a = va_arg(ap, unsigned short int) & 0xFF;
-		*a = (unsigned char)(*a);
+		a = (unsigned char)(a) & 0xFF;
 	else if (all->l_m == 3)
 		// a = va_arg(ap, unsigned long);
-		*a = (unsigned long)(*a);
+		a = (unsigned long)(a);
 	else if (all->l_m == 4)
 		// a = va_arg(ap, unsigned long long);
-		*a = (unsigned long long)(*a);
+		a = (unsigned long long)(a);
 	else if (all->l_m == 5)
 		// a = va_arg(ap, uintmax_t);
-		*a = (uintmax_t)(*a);
+		a = (uintmax_t)(a);
 	else if (all->l_m == 6)
 		// a = va_arg(ap, size_t);
-		*a = (size_t)(*a);
+		a = (size_t)(a);
 	else if (all->l_m == 0)
 		// a = va_arg(ap, unsigned int);
-		*a = (unsigned int)(*a);
-	ft_prnum(ft_itoa_base(*a, 10), *all, 'u', pd);
+		a = (unsigned int)(a);
+	ft_prnum(ft_itoa_base(a, 10), *all, 'u', pd);
 }
 
 void	ft_format_uu(va_list ap, t_printf *all, int *pd)
--- ftprintf/ft_format_x.c
+++ ftprintf/ft_format_x.c
@@ -2,31 +2,31 @@
 
 char	*ft_format_x(va_list ap, t_printf *all)
 {
-	unsigned long long int *a;
+	unsigned long long int a;
 
-	a = va_arg(ap, unsigned long long int*);
+	a = va_arg(ap, unsigned long long int);
 	if (all->l_m == 1)
 		// a = va_arg(ap, unsigned int) & 0xFFFF;
-		*a = (unsigned int)(*a);
+		a = (unsigned int)(a) & 0xFFFF;
 	else if (all->l_m == 2)
 		// a = va_arg(ap, unsigned short int) & 0xFF;
-		*a = (unsigned char)(*a);
+		a = (unsigned char)(a) & 0xFF;
 	else if (all->l_m == 3)
 		// a = va_arg(ap, unsigned long);
-		*a = (unsigned long)(*a);
+		a = (unsigned long)(a);
 	else if (all->l_m == 4)
 		// a = va_arg(ap, unsigned long long);
-		*a = (unsigned long long)(*a);
+		a = (unsigned long long)(a);
 	else if (all->l_m == 5)
 		// a = va_arg(ap, uintmax_t);
-		*a = (uintmax_t)(*a);
+		a = (uintmax_t)(a);
 	else if (all->l_m == 6)
 		// a = va_arg(ap, size_t);
-		*a = (size_t)(*a);
+		a = (size_t)(a);
 	else if (all->l_m == 0)
 		// a = va_arg(ap, unsigned int);
-		*a = (unsigned int)(*a);
-	return(ft_itoa_base(*a, 16));
+		a = (unsigned int)(a);
+	return(ft_itoa_base(a, 16));
 }
 
 void	ft_format_xx(va_list ap, t_printf *all, int *pd, char c)
