0599aca3feab710023cf30156fc32b0fdf99da9a
aahuba <huba.zp@gmail.com>
2018-06-19 14:55:40 +0300

    Add files via upload

--- ftprintf/ft_format.c
+++ ftprintf/ft_format.c
@@ -50,20 +50,18 @@ void	ft_format_d(va_list ap, t_printf *all, int *pd)
 	long long int	a;
 
 	a = 0;
-	if (all->l_m == 1)
-		a = (short int)va_arg(ap, int);
+	if (all->l_m == 0)
+		a = va_arg(ap, int);
+	else if (all->l_m == 1)
+		a = (signed char)va_arg(ap, int);
 	else if (all->l_m == 2)
-		a = (char)va_arg(ap, int);
+		a = (short)va_arg(ap, int);
 	else if (all->l_m == 3)
 		a = va_arg(ap, long);
-	else if (all->l_m == 4)
-		a = va_arg(ap, long long);
 	else if (all->l_m == 5)
 		a = va_arg(ap, intmax_t);
 	else if (all->l_m == 6)
-		a = va_arg(ap, long long int);
-	else if (all->l_m == 0)
-		a = va_arg(ap, int);
+		a = va_arg(ap, size_t);
 	ft_prnum(ft_itoa(a), *all, 'd', pd);
 }
 
