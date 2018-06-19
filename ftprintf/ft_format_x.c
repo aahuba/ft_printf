356bc4323a7af66b7a0a4d60b98502282f2cc180
aahuba <huba.zp@gmail.com>
2018-06-19 13:45:53 +0300

    Update ft_format_x.c

--- ftprintf/ft_format_x.c
+++ ftprintf/ft_format_x.c
@@ -9,7 +9,7 @@ char	*ft_format_x(va_list ap, t_printf *all)
 		a = va_arg(ap, unsigned int) & 0xFFFF;
 	else if (all->l_m == 2)
 		// a = va_arg(ap, unsigned short) & 0xFF;
-		a = (unsigned char)(a);
+		a = (unsigned char)(a) & 0xFF;
 	else if (all->l_m == 3)
 		a = va_arg(ap, unsigned long);
 	else if (all->l_m == 4)
