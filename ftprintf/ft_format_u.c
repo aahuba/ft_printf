7734730498967af95d7029a23000d9e3413cb1b3
aahuba <huba.zp@gmail.com>
2018-06-19 13:28:51 +0300

    Update ft_format_u.c

--- ftprintf/ft_format_u.c
+++ ftprintf/ft_format_u.c
@@ -8,7 +8,7 @@ void	ft_format_u(va_list ap, t_printf *all, int *pd)
 	if (all->l_m == 1)
 		a = va_arg(ap, unsigned int) & 0xFFFF;
 	else if (all->l_m == 2)
-		a = va_arg(ap, unsigned short int) & 0xFF;
+		a = va_arg(ap, unsigned int) & 0xFF;
 	else if (all->l_m == 3)
 		a = va_arg(ap, unsigned long);
 	else if (all->l_m == 4)
